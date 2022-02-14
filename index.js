7b2f30b22876bb8b12c48cdce90dedf0029782b4
Sahil Khadka <sahilkhadka767@gmail.com>
2022-02-14 09:25:53 +0545

    done

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const menuItems = document.getElementById("ham-menu-items")
 const hamMenu = document.getElementById("ham-menu")
 const bodyEl = document.querySelector("body")
 
+var hamEl = document.getElementById("ham-menu-img")
+var toggle = false
+
 hamMenu.addEventListener("click", () => {
     menuItems.classList.toggle("toggle")
-    bodyEl.classList.add("noscroll")
+    bodyEl.classList.toggle("noscroll")
+    if (toggle === true) {
+        hamEl.src = "./images/icons-ham.png"
+    } else {
+        hamEl.src = "./images/cancel.png"
+    }
+    toggle = !toggle
 })
 
 function handleLinks() {
