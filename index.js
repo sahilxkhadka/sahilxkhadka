dd4dd1bbc18eb18a6de5d72f44c95afc3ea0cd7e
Sahil Khadka <sahilkhadka767@gmail.com>
2022-02-24 20:34:09 +0545

    scroll to top added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const basicInfo = document.getElementById("basicinfo")
+const backToTop = document.getElementById("scroll-top")
 const menuItems = document.getElementById("ham-menu-items")
 const hamMenu = document.getElementById("ham-menu")
 const bodyEl = document.querySelector("body")
@@ -20,3 +22,17 @@ function handleLinks() {
     menuItems.classList.toggle("toggle")
     bodyEl.classList.remove("noscroll")
 }
+
+
+const observer = new IntersectionObserver(entries => {
+    const [entry] = entries
+    if (entry.isIntersecting) {
+        backToTop.classList.add("hide-button")
+        // observer.unobserve(basicInfo)
+    } else {
+        backToTop.classList.remove("hide-button")
+    }
+}, {
+    threshold: 1
+})
+observer.observe(basicInfo)
